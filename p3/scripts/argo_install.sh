555ac991ebaa884c2c166e2289ab52ee35589766
Bracken Domitila <batyrahaitov@gmail.com>
2022-04-11 21:01:40 +0300

    p3 added, structure fixed

---
+++ p3/scripts/argo_install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+app_file_name="/vagrant/dev/cd_test.yaml"
+argo_ingress_file="/vagrant/scripts/argo_install.sh"
+ingres_config_file="/vagrant/argo-traefic-ingress-config.yaml"
+
+kubectl create namespace argocd
+kubectl create namespace dev
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+if [ -f "$app_file_name" ]
+then
+    kubectl apply -n dev -f $app_file_name
+fi
+
+nohup kubectl port-forward svc/traefik -n kube-system 443:443 --address=0.0.0.0 > /dev/null 2>&1 &
+nohup kubectl port-forward svc/traefik -n kube-system 80:80 --address=0.0.0.0 > /dev/null 2>&1 &
+nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 --address=0.0.0.0 > /dev/null 2>&1 &
