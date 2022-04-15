06b42d60508db00c5d33a3a2ff95d89e5a929b80
Bracken Domitila <batyrahaitov@gmail.com>
2022-04-15 16:29:59 +0300

    p3 done

--- p3/scripts/argo_install.sh
+++ p3/scripts/argo_install.sh
@@ -13,6 +13,10 @@ then
     kubectl apply -n dev -f $app_file_name
 fi
 
-nohup kubectl port-forward svc/traefik -n kube-system 443:443 --address=0.0.0.0 > /dev/null 2>&1 &
-nohup kubectl port-forward svc/traefik -n kube-system 80:80 --address=0.0.0.0 > /dev/null 2>&1 &
-nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 --address=0.0.0.0 > /dev/null 2>&1 &
+while [ ! $(kubectl rollout -n argocd status deploy/argocd-server >2 /dev/null )  ] #wait while argocd-server deplot to start for forwarding its ports out
+do
+
+done
+    nohup kubectl port-forward svc/traefik -n kube-system 443:443 --address=0.0.0.0 > /dev/null 2>&1 &
+    nohup kubectl port-forward svc/traefik -n kube-system 80:80 --address=0.0.0.0 > /dev/null 2>&1 &
+    nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 --address=0.0.0.0 > /dev/null 2>&1 &
